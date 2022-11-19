08faa196b06c7fa09346d7818a88bad13d4ea8a1
ihsanvp <ihsanvp2738@gmail.com>
2022-11-20 01:04:14 +0530

    start section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,4 +3,25 @@
 </script>
 
 <Landing />
-<div class="h-screen" />
+<section class="my-60">
+	<div class="container mx-auto">
+		<div class="grid grid-cols-6">
+			<div class="flex flex-col gap-10 col-span-4">
+				<div class="text-5xl font-bold leading-[1.1]">
+					Safe, Reliable And Express Logistic & Transport Solutions That Saves Your Time!
+				</div>
+				<div class="text-xl font-medium pr-40 leading-[1.5]">
+					Logisti Group is a representative logistics operator providing full range of service in
+					the sphere of customs clearance and transportation worldwide for any type of cargo.
+				</div>
+				<div class="text-gray-600 pr-40 leading-[1.6]">
+					We pride ourselves on providing the best transport and shipping services available allover
+					the world. Our skilled personnel, utilising the latest communications, tracking and
+					processing software, combined with decades of experience! Through integrated supply chain
+					solutions, Logisti drives sustainable competitive advantages to some of Australia’s
+					largest companies.
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
