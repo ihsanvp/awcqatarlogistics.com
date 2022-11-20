cde97c5f0ea169dfce61b053d129a8e7811e6d2e
ihsanvp <ihsanvp2738@gmail.com>
2022-11-20 09:53:06 +0530

    add inter font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
+      fontFamily: {
+        serif: ["Inter", ...defaultTheme.fontFamily.serif]
+      },
       container: {
         padding: {
           DEFAULT: '1rem',
