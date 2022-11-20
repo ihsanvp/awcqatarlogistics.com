5dd1e7c2ece11c09e7bf03bdc04f8b69353f49a9
ihsanvp <ihsanvp2738@gmail.com>
2022-11-20 20:01:49 +0530

    update footer

--- src/components/footer/Footer.svelte
+++ src/components/footer/Footer.svelte
@@ -1,5 +1,8 @@
 <footer>
 	<div class="container mx-auto">
-		<hr class="border-t border-gray-400" />
+		<hr class="border-t border-gray-200" />
+		<div class="py-40">
+			<div class="">Logo</div>
+		</div>
 	</div>
 </footer>
