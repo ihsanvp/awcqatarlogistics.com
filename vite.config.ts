48085df5bdacf502f686219662c1805e0343457f
ihsanvp <ihsanvp2738@gmail.com>
2022-11-28 01:10:49 +0530

    stable add image

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		include: ['blurhash'],
+	},
+	ssr: {
+		noExternal: ['svelte-image'],
+	},
 };
 
 export default config;
