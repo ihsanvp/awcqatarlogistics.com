bab9874e94d0c229f90bdbbace32f87f451ed0b6
ihsanvp <ihsanvp2738@gmail.com>
2022-12-06 21:32:25 +0530

    add service animation

--- src/components/cards/ServiceCard.svelte
+++ src/components/cards/ServiceCard.svelte
@@ -1,7 +1,13 @@
-<div class="flex flex-col gap-4 relative items-center justify-center sm:items-start">
+<div class="flex flex-col gap-4 relative items-center justify-center sm:items-start group">
 	<div class="w-[100px] fill-secondary relative">
-		<div class="absolute -top-5 -left-5 w-20 h-20 bg-secondary opacity-10 z-[-1]" />
-		<slot name="icon" />
+		<div
+			class="absolute -top-5 -left-5 w-20 h-20 bg-secondary opacity-10 z-[-1] duration-500 transition-transform md:group-hover:-translate-x-5 md:group-hover:-translate-y-5 md:group-hover:rotate-12"
+		/>
+		<div
+			class="duration-500 transition-transform md:group-hover:translate-x-5 md:group-hover:-rotate-12"
+		>
+			<slot name="icon" />
+		</div>
 	</div>
 	<div class="text-2xl font-black title text-center sm:text-left">
 		<slot name="title" />
